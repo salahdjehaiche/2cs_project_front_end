038c673cf1f4627f07f288ee593434c98b195b9f
salahdjehaiche <hs_djehaiche@esi.dz>
2021-06-14 01:20:03 +0200

    therteeneth commit

--- src/App.vue
+++ src/App.vue
@@ -27,4 +27,31 @@ export default {
 #nav a.router-link-exact-active {
   color: #0077ff;
 }
+#nav a.router-link-exact-active p{
+ display:inherit;
+    border-bottom:1px solid rgb(160, 158, 158);
+    padding-bottom:1px;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 8px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px #ccc; 
+  border-radius: 10px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #aaa; 
+  border-radius: 5px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #888; 
+}
 </style>
--- src/components/WelcomeSidebar.vue
+++ src/components/WelcomeSidebar.vue
@@ -1,11 +1,11 @@
 <template>
     <!-- give the sidebar z-50 class so its higher than the navbar if you want to see the logo -->
     <!-- you will need to add a little "X" button next to the logo in order to close it though -->
-    <div @click.self="toggleSidebar()"    class="overflow-y-auto bg-gray-800 w-full md:w-1/2 opacity-50 
+    <div @click.self="toggleSidebar()"    class="md:rotate overflow-y-auto bg-gray-800 w-full md:w-1/2 bg-opacity-75 
     md:opacity-100   lg:w-72 fixed  md:top-15 lg:top-0 top-0 md:left-0 h-screen lg:block md:bg-white border-r-2  z-30" 
      :class="sideBarOpen ? '' : 'hidden'" id="main-nav">
-      <div class="bg-white w-4/5 md:w-full opacity-100 z-10">
-          <div class="w-full   flex px-4 items-center mb-8 ">
+      <div class="bg-white w-3/4 sm:w-1/2 md:w-full opacity-100 z-10">
+          <div class="w-2/3   flex px-4 items-center mb-8 ">
             <img src="../assets/logo.png" alt="2a logo">
           </div>
           <div class="mb-4 px-4 text-blue-800" id="nav" v-for="link in  sideBar" :key="link" >
@@ -19,7 +19,7 @@
                       <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                     </svg>
                   </div>
-                  {{link.label}} 
+                  <p>{{link.label}} </p>
               </div>
             </router-link> 
 
@@ -47,3 +47,13 @@ export default {
     }
 }
 </script>
+<style >
+
+.rotate {
+   direction: rtl;
+  overflow-y: scroll;
+  }
+.rotate div{
+  direction: ltr;
+}
+</style>
